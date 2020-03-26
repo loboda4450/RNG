811588aeb26adc726c10d60d9ad08f09c8166cbe
loboda4450 <loboda4450@gmail.com>
2020-03-26 15:06:23 +0100

    added range of generated number

--- RNGutils.py
+++ RNGutils.py
@@ -10,36 +10,38 @@ class RNGutils:
         self.l0 = None
         self.m = None
 
-    def single_handler(self, lower_prime, higher_prime):
+    def single_handler(self, lower_prime, higher_prime, rnd_range):
         try:
             self.m = lower_prime * higher_prime
             self.b0 = (lower_prime ** 2) % self.m
             self.d0 = (higher_prime ** 2) % self.m
-            self.f0 = self.b0 * self.d0 * int(time.time())
+            self.l0 = int(time.time())*1000
+            self.f0 = self.b0 * self.d0 * int(time.time()) % rnd_range
         except Exception as e:
             print(e)
 
-    def sequention_handler(self, primes, sequention_length):
+    def sequention_handler(self, primes, sequention_length, rnd_range):
         for i in range(int(sequention_length)):
             try:
                 self.m = primes.lower_prime * primes.higher_prime
                 self.b0 = (self.b0 ** 2) % self.m
                 self.d0 = (self.d0 ** 2) % self.m
-                self.f0 = self.b0 * self.d0 * int(time.time())
+                self.f0 = self.b0 * self.d0 * self.l0 % rnd_range
                 self.random_array.append(self.f0)
+                time.sleep(0.5)
             except Exception as e:
                 print(e)
         pass
 
-    def single(self, primes, pixel_seed):
+    def single(self, primes, pixel_seed, rnd_range):
         primes.getFirstHigher(seed=pixel_seed)
         primes.getFirstLower(seed=pixel_seed)
-        self.single_handler(lower_prime=primes.lower_prime, higher_prime=primes.higher_prime)
+        self.single_handler(lower_prime=primes.lower_prime, higher_prime=primes.higher_prime, rnd_range=rnd_range)
         return int(self.f0)
 
-    def sequention(self, primes, pixel_seed, length):
+    def sequention(self, primes, pixel_seed, length, rnd_range):
         primes.getFirstHigher(seed=pixel_seed)
         primes.getFirstLower(seed=pixel_seed)
-        self.single_handler(lower_prime=primes.lower_prime, higher_prime=primes.higher_prime)
-        self.sequention_handler(primes=primes, sequention_length=length)
+        self.single_handler(lower_prime=primes.lower_prime, higher_prime=primes.higher_prime, rnd_range=rnd_range)
+        self.sequention_handler(primes=primes, sequention_length=length, rnd_range=rnd_range)
         return self.random_array
--- rng.py
+++ rng.py
@@ -6,6 +6,27 @@ import Primes
 import RNGutils
 
 
+def check_sysargs(sysargs):
+    if sysargs[1] == '--single':
+        args = 3
+        if len(sysargs) != args:
+            print(f'You provided {len(sysargs) - 1} out of {args} required arguments')
+            sys.exit()
+        else:
+            print(f'mode: {sysargs[1]},\nsize of sequence: {sysargs[2]},\nrandom number range: {sysargs[3]}')
+            pass
+    elif sysargs[1] == '--sequention':
+        args = 4
+        if len(sysargs) != args:
+            print(f'You provided {len(sysargs) - 1} out of {args} required arguments')
+            sys.exit()
+        else:
+            print(f'mode: {sysargs[1]},\nrandom number range: {sysargs[2]}')
+            pass
+    else:
+        print(f'Wrong mode "{sysargs[1]}" (available --single, --sequention)')
+
+
 def get_seed_from_pixel():
     image = ImageGrab.grab()
     w, h = image.size
@@ -18,12 +39,15 @@ def get_seed_from_pixel():
 
 
 def main():
+    check_sysargs(sys.argv)  # check if user defined all required parameters
+
     primes = Primes.Primes()
     rng = RNGutils.RNGutils()
     seed = get_seed_from_pixel()
     if sys.argv[1] == "--sequention":
+        print("Sequention mode\n")
         with open("output.txt", "a+") as file:
-            random_array = rng.sequention(primes, seed, sys.argv[2])
+            random_array = rng.sequention(primes=primes, pixel_seed=seed, length=sys.argv[2], rnd_range=int(sys.argv[3]))
             for number in random_array:
                 file.write(f'{number}\n')
                 print(number)
@@ -31,9 +55,9 @@ def main():
         file.close()
 
     elif sys.argv[1] == "--single":
-        print("Chosen single\n")
+        print("Single mode\n")
         with open("output.txt", "a+") as file:
-            string = f'{rng.single(primes=primes, pixel_seed=seed)}\n'
+            string = f'{rng.single(primes=primes, pixel_seed=seed, rnd_range=int(sys.argv[2]))}\n'
             if "-" not in string:
                 file.write(string)
                 print(string)
